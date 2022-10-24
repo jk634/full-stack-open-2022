dbf7d49206e7a7f0f3fa5a7e76ed86fc6ace4c9a
Jussi <jussi.kanto@tuni.fi>
2022-10-24 10:33:35 +0300

    Add 6.13 - anekdootit ja backend, step1

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import anecdotes from './services/anecdotes';
+import { setAnecdotes } from './reducers/anecdoteReducer';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdotes.getAll().then((anec) => dispatch(setAnecdotes(anec)));
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
