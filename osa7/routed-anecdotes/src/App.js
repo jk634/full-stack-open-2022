2014b86cdfb498c1e45215128dce88b85cc0656d
Jussi <jussi.kanto@tuni.fi>
2022-10-25 11:48:32 +0300

    Add 7.5 - anekdoottisovellus ja hookit step2

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -98,6 +98,13 @@ const CreateNew = (props) => {
     navigate('/');
   };
 
+  const handleReset = (event) => {
+    event.preventDefault();
+    content.reset();
+    author.reset();
+    info.reset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -114,7 +121,8 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   );
